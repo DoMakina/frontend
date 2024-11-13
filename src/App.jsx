f5b3a00218c676755a9c67fb04aebdc6ec82942c
ElisBushaj <elisbushaj2@gmail.com>
2024-11-13 14:45:11 +0100

    initialize React app with Vite, Tailwind CSS, and ESLint configuration

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="flex h-screen w-screen items-center justify-center bg-black text-white">
+			<h1 className="text-4xl">Hello World</h1>
+		</div>
+	);
+}
+
+export default App;
