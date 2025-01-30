93ac2e89e3576ed0323904aee7af6dfbc346cf8b
ElisBushaj <elisbushaj2@gmail.com>
2025-01-30 19:45:19 +0100

    Add AdminBrandsPage with MainLayout integration

---
+++ src/pages/private/admin/AdminBrandsPage.jsx
@@ -0,0 +1,11 @@
+import { MainLayout } from "../../../components/layouts";
+
+const AdminBrandsPage = () => {
+	return (
+		<MainLayout mainOptions={{ paddingVertical: false }}>
+			<h1>Admin Brands</h1>
+		</MainLayout>
+	);
+};
+
+export default AdminBrandsPage;
